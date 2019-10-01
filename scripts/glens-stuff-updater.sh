4233f90e194f00452536f968f143432be2dbf14f
theglennewman <15897605+theglennewman@users.noreply.github.com>
2019-10-01 14:30:00 -0400

    updates to the updater

--- scripts/glens-stuff-updater.sh
+++ scripts/glens-stuff-updater.sh
@@ -8,4 +8,18 @@ dstDir="${HOME}/github-projects/glens-stuff"
 
 # source as first item, destination in git as second item
 thefiles=()
-thefiles+=("~/.bashrc"
+thefiles+=("~/.bashrc")
+
+# make sure the repo doesn't have any pending changes first
+cd ${dstDir}
+git status 2>&1 | grep -qs 'nothing to commit, working tree clean'
+gitStatCode="$?"
+if [[ "${gitStatCode}" == "1" ]]; then
+  echo
+  echo "Git repo has changes pending: ${dstDir}"
+  echo "We refuse to continue. Commit changes first before pulling updates in."
+  echo
+  exit 123
+fi
+
+else "We'll totally copy stuff in now"
