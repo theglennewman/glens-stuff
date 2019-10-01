6121ef090bd2f0cdd30c90474fb98b6e2f2f92c8
theglennewman <15897605+theglennewman@users.noreply.github.com>
2019-10-01 15:02:00 -0400

    working confs

--- scripts/glens-stuff-updater.sh
+++ scripts/glens-stuff-updater.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-# Copies various files to my glens-stuff git repo and runs git status.
+# Copies various files to my local glens-stuff git repo and runs git status.
 #
 # gnewman 2019.10.01
 
-dstDir="${HOME}/github-projects/glens-stuff"
-
-# source as first item, destination in git as second item
-thefiles=()
-thefiles+=("~/.bashrc")
+gitDir="${HOME}/github-projects/glens-stuff"
+configFiles=".bashrc .bash_profile .tmux.conf"
 
 # make sure the repo doesn't have any pending changes first
 cd ${dstDir}
 git status 2>&1 | grep -qs 'nothing to commit, working tree clean'
-gitStatCode="$?"
+#gitStatCode="$?"
+gitStatCode="0" # for now, while making changes, ignore the git status
 if [[ "${gitStatCode}" == "1" ]]; then
   echo
-  echo "Git repo has changes pending: ${dstDir}"
+  echo "Git repo has changes pending: ${gitDir}"
   echo "We refuse to continue. Commit changes first before pulling updates in."
   echo
   exit 123
 fi
 
-echo "We'll totally copy stuff in now"
+# arg 1 = source dir
+# arg 2 = dest dir
+# arg 3+ = files to copy
+copyworker () {
+  srcDir="$1"
+  dstDir="$2"
+  shift 2
+  files="$@"
+  echo "${srcDir} => ${dstDir}"
+  for xx in ${files}; do
+  	src="${srcDir}/${xx}"
+  	dst="${dstDir}/${xx}"
+  	echo "  ${src} => ${dst}"
+  	cp ${src} ${dst}
+  done
+}
+
+copyworker ${HOME} ${gitDir}/configs ${configFiles}
+
+# finally...
+cd ${gitDir}
+echo
+git status
