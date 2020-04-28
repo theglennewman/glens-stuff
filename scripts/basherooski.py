b4f0fec54b6625fda70e8d3199fdb8cc5450a720
theglennewman <15897605+theglennewman@users.noreply.github.com>
2020-04-27 22:43:07 -0400

    changes to comments

--- scripts/basherooski.py
+++ scripts/basherooski.py
@@ -3,25 +3,19 @@
 
 import subprocess
 
-# This is it! I hate cooking this up every time...
-# Runs a bash command. Returns a string. (Or you can set return_code to True
-# and return the command's exit code)
-
-# Look. I just hate having to write this every dang time. And recently I found
-# out they made a run function! And it's absolutely lovely. This is just a
-# wrapper to that, but we always convert the command to a string and set the
-# encoding to utf-8.
-#
 # Returns the subprocess.CompletedProcess object with these fields:
 #   - args (used to launch it)
 #   - returncode (exit status of the command)
 #   - stdout
 #   - stderr
-#
 # Raises the CalledProcessError if the command returned a non-zero code.
 def bash_it_up(command):
+  # there may be a better way to process / sanitize the command input... maybe
+  # something in shlex as recommended in the subprocess doc? I'm calling with
+  # shell = True but maybe I shouldn't
+  more_gooder_command = str(command)
   return subprocess.run(
-    str(command),
+    more_gooder_command,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
     shell=True,
