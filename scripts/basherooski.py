283db6b9affa6253cf0a0d8583c9b228cb8bc7af
theglennewman <15897605+theglennewman@users.noreply.github.com>
2020-10-30 17:50:58 -0400

    i'm pretty happy with this bash thingy for python

--- scripts/basherooski.py
+++ scripts/basherooski.py
@@ -3,24 +3,24 @@
 
 import subprocess
 
-# Returns the subprocess.CompletedProcess object with these fields:
-#   - args (used to launch it)
-#   - returncode (exit status of the command)
-#   - stdout
-#   - stderr
-# Raises the CalledProcessError if the command returned a non-zero code.
-def bash_it_up(command):
-  # there may be a better way to process / sanitize the command input... maybe
-  # something in shlex as recommended in the subprocess doc? I'm calling with
-  # shell = True but maybe I shouldn't
+# WHAT:   runs specified command (thru "sh" via subprocess)
+# RETURN: subprocess.CompletedProcess -> (args, returncode, stdout, stderr)
+# RAISE:  CalledProcessError if non-zero returned... unless check is False
+# NOTE: input may be unsafe? command converted to string, not sanitized... so
+#       don't do user input direct to this because shell=True is used. Maybe
+#       later we'll use "shlex" as recommended in python's subprocess doc
+#
+# also... this requires python 3.6... make sure to use: #!/usr/bin/python3
+def basherooski(command, check=True, stdout=None, stderr=None):
   more_gooder_command = str(command)
-  return subprocess.run(
-    more_gooder_command,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    shell=True,
-    check=True,
-    encoding="utf-8"
-  )
-
-bash_it_up("ls -lah") #lol I need to install python with cygwin...
+  return subprocess.run(more_gooder_command, shell=True, encoding="utf-8",
+    check=check, stdout=stdout, stderr=stderr)
+# no waiting, output is "live" (stdout and sterr go to parent handles)
+def bash_live(command):
+  return basherooski(command)
+# wait then return completed process (stdout and stderr are in returned object)
+def bash_complete(command):
+  return basherooski(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# run quiet, then return JUST the stdout (good for variables... but no stderr)
+def bash_str(command):
+  return bash_complete(command).stdout
